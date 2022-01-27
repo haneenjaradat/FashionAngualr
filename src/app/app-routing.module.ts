45602d8ed148aeba289ce1731c4d59eaf0bca3aa
Haneen <haneenjaradat06@gmail.com>
2022-01-27 23:40:17 +0200

    first push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './HomePage/home/home.component';
+import { HomeModuleModule } from './module/home-module/home-module.module';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',loadChildren:()=>(import('./module/home-module/home-module-routing.module').then((x)=>x.HomeModuleRoutingModule))}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/module/home-module/home-module-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from 'src/app/about/about.component';
+import { ContactComponent } from 'src/app/contact/contact.component';
+import { HomeComponent } from 'src/app/HomePage/home/home.component';
+
+const routes: Routes = [{path:'',component:HomeComponent},{path:'About',component:AboutComponent},{path:'Contact',component:ContactComponent}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeModuleRoutingModule { }
